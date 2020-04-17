30584059381dc83793a775174c69424c12a06b3d
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-04-17 11:52:15 -0700

    created makeUser function and TDD successful

---
+++ make-user.js
@@ -0,0 +1,14 @@
+export default function makeUser(_formData){ 
+    const name = _formData.get('name'); 
+    const userClass = _formData.get('class'); 
+
+    const userObject = { 
+        name: name,
+        class: userClass,
+        completed: {},
+        HP: 50, 
+        tigers: 0, 
+    };
+
+    return userObject;
+}
--- test/example.test.js
+++ test/example.test.js
@@ -1,7 +1,33 @@
 // IMPORT MODULES under test here:
 // import example from '../src/example.js';
+import makeUser from '../make-user.js'; 
 
 const test = QUnit.test;
+
+test('makes a user object in plain js from form data', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const formData = new FormData(); 
+
+    formData.set('name', 'breeann');
+    formData.set('class', 'zoo');
+
+    const expected = {
+        name: 'breeann',
+        class: 'zoo',
+        completed: {},
+        HP: 50, 
+        tigers: 0, 
+    };
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const userObject = makeUser(formData);
+    //Assert
+    // Make assertions about what is expected valid result
+    assert.deepEqual(expected, userObject);
+});
+
+
 
 test('time to test a function', function(assert) {
     //Arrange
@@ -12,5 +38,6 @@ test('time to test a function', function(assert) {
 
     //Assert
     // Make assertions about what is expected valid result
-    assert.equal(true, false);
+    assert.equal(true, true);
 });
+
