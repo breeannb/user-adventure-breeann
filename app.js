df68ec36a7f5d66ae4366623f939aa22e5981656
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-04-17 12:07:59 -0700

    added user to local storage successfully

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-
+// imports 
+import makeUser from './make-user.js'; 
 
 // get the form from the dom 
 const myForm = document.getElementById('user-form'); 
@@ -9,8 +10,15 @@ myForm.addEventListener('submit', (event) => {
     // always prevent default for the form 
     event.preventDefault(); 
 
-    const form = new FormData(myForm); // now we havwe a form 
+    const formData = new FormData(myForm); // now we have a form 
 
     // console.log(form.get('class'), form.get('name')); 
+
+
+    const user = makeUser(formData); 
+
+    // stringify and put to local storage 
+    const stringyUser = JSON.stringify(user); 
+    localStorage.setItem('USER', stringyUser);
 
 }); 
